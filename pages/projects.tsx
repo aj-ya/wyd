dae9f534eb4626d9c014588f0464df0aeafc2ce5
ajeya <ajeyabhat.off@gmail.com>
2022-07-11 23:17:26 +0530

    added projects and who is sections

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,8 +1,7 @@
-import { ExternalLinkIcon } from '@chakra-ui/icons';
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Accordion } from '@chakra-ui/react';
 import { NextPage } from 'next';
-import NextLink from 'next/link';
-import { Link } from '@chakra-ui/react';
+import Project from '../components/accordianItem';
+import ProjectData from '../lib/projects.json';
 const Projects: NextPage = () => {
     return (
         <Flex
@@ -10,19 +9,19 @@ const Projects: NextPage = () => {
             height="100%"
             alignItems={'center'}
             justifyContent={'center'}
+            color={'cooler.secondaryText'}
+            fontFamily={'monospace'}
+            fontSize={['1em', '1.1em', '1.2em']}
         >
-            WIP - head over to my github for a peek into my past projects.
-            <NextLink href="https://outpost.run" target={'_blank'} passHref>
-                <Link
-                    textDecoration={'underline'}
-                    textDecorationStyle={'dotted'}
-                    target={'_blank'}
-                    display={['block', 'inline']}
-                    color="cooler.text"
-                >
-                    Outpost <ExternalLinkIcon mx="2px" />
-                </Link>
-            </NextLink>
+            <Accordion
+                defaultIndex={[0]}
+                allowMultiple
+                width={['70%', '60%', '50%']}
+            >
+                {ProjectData.map((project) => (
+                    <Project key={project.title} projectDetails={project} />
+                ))}
+            </Accordion>
         </Flex>
     );
 };
--- pages/whois.tsx
+++ pages/whois.tsx
@@ -1,8 +1,17 @@
-import { Box, Flex } from '@chakra-ui/react';
+import {
+    Avatar,
+    Box,
+    Flex,
+    HStack,
+    TagRightIcon,
+    Text,
+    VStack,
+} from '@chakra-ui/react';
 import { NextPage } from 'next';
 import NextLink from 'next/link';
 import { Link } from '@chakra-ui/react';
 import NavLink from '../components/navLink';
+import { ArrowRightIcon } from '@chakra-ui/icons';
 const WhoIs: NextPage = () => {
     return (
         <Flex
@@ -11,14 +20,97 @@ const WhoIs: NextPage = () => {
             py={4}
             alignItems={'center'}
             justifyContent={'center'}
+            fontFamily={'monospace'}
         >
-            view my{' '}
-            <NavLink
-                linkObj={{ name: 'resume', to: '/resume.pdf' }}
-                textDecoration={'underline'}
-                textDecorationStyle={'wavy'}
-            />
-            .
+            <VStack
+                width={['60%', '40%', '30%']}
+                border="1px"
+                borderRadius={'10px'}
+                borderColor={'cooler.secondaryText'}
+                color={'cooler.secondaryText'}
+                alignItems="center"
+                justifyContent={'start'}
+                p={6}
+            >
+                <HStack
+                    width="100%"
+                    display={'flex'}
+                    justifyContent={'space-between'}
+                >
+                    <Box
+                        color={'cooler.text'}
+                        fontWeight={'extrabold'}
+                        fontSize={['xl', '2xl', '3xl']}
+                    >
+                        Ajeya Bhat
+                    </Box>
+                    <Avatar
+                        name="Ajeya Bhat"
+                        src="/pfp-github.jpeg"
+                        size={['md']}
+                    />
+                </HStack>
+                <Flex
+                    width="100%"
+                    direction={'row'}
+                    justifyContent={'space-between'}
+                    wrap={'wrap'}
+                    gap={'2'}
+                    fontSize={['1em', '1.2em']}
+                    py={'2'}
+                >
+                    <NavLink
+                        linkObj={{
+                            name: 'Steam',
+                            to: 'https://steamcommunity.com/id/sh4n1/',
+                        }}
+                        textDecoration={'underline'}
+                        textDecorationStyle={'wavy'}
+                        isExternal
+                        _hover={{ color: 'cooler.primary' }}
+                    />
+                    <NavLink
+                        linkObj={{
+                            name: 'GitHub',
+                            to: 'https://github.com/aj-ya',
+                        }}
+                        textDecoration={'underline'}
+                        textDecorationOffsetBottom={'2px'}
+                        textDecorationStyle={'wavy'}
+                        isExternal
+                        _hover={{ color: 'cooler.primary' }}
+                    />
+                    <NavLink
+                        linkObj={{
+                            name: 'LinkedIn',
+                            to: 'https://linkedin.com/in/aj-ya',
+                        }}
+                        textDecoration={'underline'}
+                        textDecorationStyle={'wavy'}
+                        _hover={{ color: 'cooler.primary' }}
+                        isExternal
+                    />
+                    <NavLink
+                        linkObj={{ name: 'Resume', to: '/resume.pdf' }}
+                        textDecoration={'underline'}
+                        textDecorationStyle={'wavy'}
+                        _hover={{ color: 'cooler.primary' }}
+                    />
+                </Flex>
+                <Text
+                    fontSize={['1em', '1.4em']}
+                    _selection={{
+                        background: 'cooler.text',
+                        color: 'cooler.background',
+                    }}
+                >
+                    <ArrowRightIcon />
+                    &nbsp;I&apos;m a diligent computer science undergraduate
+                    student, currently pursuing B.E. at KLE Technological
+                    University with a passion for Free and Open-Source
+                    Software(FOSS) and Web and Cloud-Based Technologies.
+                </Text>
+            </VStack>
         </Flex>
     );
 };
